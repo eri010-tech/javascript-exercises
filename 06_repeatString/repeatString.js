75acaeef83fb069a80d4923e8f2c3ce340c65b8f
Mya <211755169+eri010-tech@users.noreply.github.com>
2025-11-11 15:26:51 -0800

    not sure if this is right

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) { 
+  for (let i = num; i < 4; i++) {
+    const duplicateString = string; 
+    let result = duplicateString++;
+    return result;  
+  }
 };
 
 // Do not edit below this line
