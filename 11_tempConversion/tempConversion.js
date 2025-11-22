f77761c5534577f5e60af918daca1859c9713ba5
Mya <211755169+eri010-tech@users.noreply.github.com>
2025-11-21 21:08:56 -0800

    feeling better

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+/* Odin Project's answer: 
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+}; */
+
+// below is my answer, which I prefer because it requires less steps :) 
+const convertToCelsius = function(fahrenheit) {
+  let celsiusConversion = (fahrenheit - 32) / 1.8;
+  let conversionAnswer = Math.round(celsiusConversion); 
+  return conversionAnswer; 
+};
+
+const convertToFahrenheit = function(celsius) {
+  let fahrenheitConversion = (celsius * 1.8) + 32; 
+  let conversionAnswer = Math.round(fahrenheitConversion); 
+  return conversionAnswer; 
 };
 
 // Do not edit below this line
